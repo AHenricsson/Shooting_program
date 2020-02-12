34eb4b3edd79a0ccc5ef2986bfc2ee8c9e4b9ea1
Anders <anders.henricsson@gmail.com>
2020-02-12 23:55:51 +0100

    updated for festival

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,7 @@
-apt install -y espeak
-apt install -y python-pip3
-pip3 install pyttsx3
+#!/bin/bash
 
+#apt install -y espeak
+#apt install -y python-pip3
+#pip3 install pyttsx3
+
+apt install -y festival
